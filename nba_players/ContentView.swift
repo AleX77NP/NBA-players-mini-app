0d7813061796fcc50747755ffcd9aae28bb3430a
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2020-09-27 12:46:46 +0200

    Initial Commit

---
+++ nba_players/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nba_players
+//
+//  Created by Aleksandar on 27/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nba_players/nba_playersApp.swift
@@ -0,0 +1,17 @@
+//
+//  nba_playersApp.swift
+//  nba_players
+//
+//  Created by Aleksandar on 27/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct nba_playersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
